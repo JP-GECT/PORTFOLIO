ed4232d1830dd5fe31b30c36743711fca09f1ad7
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-04-13 14:09:17 +0530

    made navbar responsive for mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import {navLinks} from '../constants'
 
 
 const Navbar = () => {
-  const[active, setActive] = useState(' ')
+  const[active, setActive] = useState(' ');
+  const[toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -30,6 +31,25 @@ const Navbar = () => {
             <a href={`#${Link.id}`}>{Link.title}</a>
           </li>))}
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle?close:menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' 
+          onClick={()=>setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden':'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <ul className='list-none flex  justify-end items-start flex-col gap-4'>
+          {navLinks.map((Link)=>(
+          <li
+          key={Link.id}
+          className={`${active===Link.title ? "text-white":"text-secondary"} font-medium text-[16px] 
+           cursor-pointer`}
+          onClick={()=>{setActive(Link.title);
+          setToggle(!toggle);}}
+          >
+            <a href={`#${Link.id}`}>{Link.title}</a>
+          </li>))}
+        </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
