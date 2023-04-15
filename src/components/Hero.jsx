e9a1b46f9ae87ab5ec4f53947175f438a088cea1
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-04-16 00:50:32 +0530

    made a higher order component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         </div>
       </div>
         <ComputersCanvas />
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
             <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
               <motion.div 
@@ -31,7 +31,7 @@ const Hero = () => {
             </div>
 
           </a>
-        </div>
+        </div> */}
       
       </section>
   )
