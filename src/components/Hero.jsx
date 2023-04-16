72395ef88921a912165375f2f5e564ebb46f48c6
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-04-17 00:01:10 +0530

    deleted hero 3d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {motion} from 'framer-motion';
 import {styles} from '../style';
-import { ComputersCanvas } from './canvas';
+//import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
@@ -15,7 +15,7 @@ const Hero = () => {
           and i am also working on three.js development</p>
         </div>
       </div>
-        <ComputersCanvas />
+       
         {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
             <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
